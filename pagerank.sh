27974d29843281a10c46d4705f025e9fe7ac1d7b
Clayton Davis <claytondus@gmail.com>
2016-04-17 11:28:50 -0400

    Pagerank script add reducer

--- pagerank.sh
+++ pagerank.sh
@@ -7,7 +7,8 @@
 hadoop fs -mkdir hdfs/pa3/pagerank
 hadoop fs -rm -R hdfs/pa3/pagerank
 hadoop jar $HADOOP_STREAMING \
-  -files pagerank_map.py \
+  -files pagerank_map.py,pagerank_reduce.py \
   -mapper pagerank_map.py \
+  -reducer pagerank_reduce.py \
   -input hdfs/pa3/parse/part-00000 \
   -output hdfs/pa3/pagerank
