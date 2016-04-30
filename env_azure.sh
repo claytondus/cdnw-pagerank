a8d8fd821409f9c758384ba3c8b631dc5a4c5ef3
Clayton Davis <cd@ae4ax.net>
2016-04-30 20:30:20 +0000

    Fix env_azure HADOOP_STREAMING

--- env_azure.sh
+++ env_azure.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-export HADOOP_STREAMING=HADOOP_STREAMING=/usr/hdp/current/hadoop-mapreduce-client/hadoop-streaming.jar
+export HADOOP_STREAMING=/usr/hdp/current/hadoop-mapreduce-client/hadoop-streaming.jar
 export HDFS_PATH=/user/$USER
